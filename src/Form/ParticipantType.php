33b5b92d18150f5da173f26e99a66db479cc4956
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2020-02-21 16:06:05 +0000

    Gestion des participants

---
+++ src/Form/ParticipantType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Participant;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ParticipantType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('libelle', TextType::class,['attr'=>['class'=>'form-control', 'placeholder'=>'Participant', 'autocomplete'=>"off"]])
+            ->add('statut',ChoiceType::class,[
+                'attr'=>['class'=>'form-control select2',],
+                'choices'=>[
+                    'Jeune'=>'JEUNE',
+                    'Adulte'=> 'ADULTE'
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Participant::class,
+        ]);
+    }
+}
